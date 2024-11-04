eaf64a29571374f3e90160e52f3e30c653ab750a
EliasKho <elias.khodjaoui@free.fr>
2024-11-04 22:38:36 +0100

    corection problemes

---
+++ src/loader/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 's3/deefy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '616e0001459a4678c34b8b8ffca12feb90119578',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        's3/deefy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '616e0001459a4678c34b8b8ffca12feb90119578',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
