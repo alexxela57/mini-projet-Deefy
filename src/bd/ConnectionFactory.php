b3690ebb9ec1d9fc49c1c9f98455730ee2eb1f24
alexxela57 <alexandre.hary3@etu.univ-lorraine.fr>
2024-11-04 15:58:05 +0100

    ajout de la connection à la bd

---
+++ src/bd/ConnectionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace deefy\bd;
+
+class ConnectionFactory{
+    public static array $config;
+    public static \PDO $connexion;
+
+    public static function setConfig($file): void
+    {
+        if(isset(self::$config)===false){
+            self::$config=parse_ini_file($file);
+        }
+    }
+
+    public static function makeConnection(): \PDO
+    {
+        if(isset(self::$connexion)===false){
+            self::$connexion=new \PDO(self::$config["driver"].":host=".self::$config["hostname"].";dbname=".self::$config["dbname"],self::$config['username'],self::$config['password']);
+        }
+
+        return self::$connexion;
+    }
+}
